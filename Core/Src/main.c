cb91371158d19c9ec4f2e1d8cfeda971b15335b3
mgupta27 <mihir_gupta@rocketmail.com>
2024-10-07 12:04:49 -0400

    Updated test code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -106,10 +106,10 @@ int main(void)
     /* USER CODE BEGIN 3 */
 		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);
 		 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
-		 HAL_Delay(1000);
+		 HAL_Delay(250);
 		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
-		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
-		 HAL_Delay(1000);
+		 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+		 HAL_Delay(250);
 
 
 
