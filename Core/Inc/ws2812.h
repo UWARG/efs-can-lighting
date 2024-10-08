970756ab580d18d6bb3f85118076f9fc49d1ec61
Anni <a26luo@uwaterloo.ca>
2024-10-08 14:56:40 -0400

    Static colour assignnments to all LED's

--- Core/Inc/ws2812.h
+++ Core/Inc/ws2812.h
@@ -15,6 +15,10 @@
 #define NUM_PIXELS (3)
 #define NUM_BYTES (NUM_BPP * NUM_PIXELS)
 
+// Constants
+#define PWM_HI (38)
+#define PWM_LO (19)
+
 // Function Declarations
 void led_set_RGB(uint8_t index, uint8_t r, uint8_t g, uint8_t b);
 void led_set_RGBW(uint8_t index, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
