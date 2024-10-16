5366ef76a7de0e5d9987658292dbc9ea4eed0872
n-arms <75282394+n-arms@users.noreply.github.com>
2024-10-16 15:54:27 -0400

    Add setup code for can interface

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -45,6 +45,7 @@ CAN_HandleTypeDef hcan1;
 TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN PV */
+static uint8_t memory_pool[1024];
 
 /* USER CODE END PV */
 
@@ -59,6 +60,308 @@ static void MX_TIM1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+
+static struct uavcan_protocol_NodeStatus node_status;
+
+// Custom clock_gettime function for STM32
+int clock_gettime(clockid_t clk_id, struct timespec *tp) {
+    if (clk_id == CLOCK_MONOTONIC) {
+        uint32_t millis = HAL_GetTick();  // Get the current tick in milliseconds
+        tp->tv_sec = millis / 1000;       // Convert milliseconds to seconds
+        tp->tv_nsec = (millis % 1000) * 1000000;  // Convert remaining milliseconds to nanoseconds
+        return 0;  // Success
+    }
+    return -1;  // Unsupported clock ID
+}
+
+/*
+  get a 16 byte unique ID for this node, this should be based on the CPU unique ID or other unique ID
+ */
+void getUniqueID(uint8_t id[16]){
+	uint32_t HALUniqueIDs[3];
+// Make Unique ID out of the 96-bit STM32 UID and fill the rest with 0s
+	memset(id, 0, 16);
+	HALUniqueIDs[0] = HAL_GetUIDw0();
+	HALUniqueIDs[1] = HAL_GetUIDw1();
+	HALUniqueIDs[2] = HAL_GetUIDw2();
+	memcpy(id, HALUniqueIDs, 12);
+}
+
+
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs) {
+	// Receiving
+	CanardCANFrame rx_frame;
+
+	const uint64_t timestamp = HAL_GetTick() * 1000ULL;
+	const int16_t rx_res = canardSTM32Recieve(hfdcan, FDCAN_RX_FIFO0, &rx_frame);
+
+	if (rx_res < 0) {
+		printf("Receive error %d\n", rx_res);
+	}
+	else if (rx_res > 0)        // Success - process the frame
+	{
+		canardHandleRxFrame(&canard, &rx_frame, timestamp);
+	}
+}
+
+// NOTE: All canard handlers and senders are based on this reference: https://dronecan.github.io/Specification/7._List_of_standard_data_types/
+// Alternatively, you can look at the corresponding generated header file in the dsdlc_generated folder
+
+// Canard Handlers ( Many have code copied from libcanard esc_node example: https://github.com/dronecan/libcanard/blob/master/examples/ESCNode/esc_node.c )
+
+void handle_NodeStatus(CanardInstance *ins, CanardRxTransfer *transfer) {
+	struct uavcan_protocol_NodeStatus nodeStatus;
+
+	if (uavcan_protocol_NodeStatus_decode(transfer, &nodeStatus)) {
+		return;
+	}
+
+	printf("Node health: %ud Node Mode: %ud\n", nodeStatus.health, nodeStatus.mode);
+
+	printf("Node Health ");
+
+	switch (nodeStatus.health) {
+	case UAVCAN_PROTOCOL_NODESTATUS_HEALTH_OK:
+		printf("OK\n");
+		break;
+	case UAVCAN_PROTOCOL_NODESTATUS_HEALTH_WARNING:
+		printf("WARNING\n");
+		break;
+	case UAVCAN_PROTOCOL_NODESTATUS_HEALTH_ERROR:
+		printf("ERROR\n");
+		break;
+	case UAVCAN_PROTOCOL_NODESTATUS_HEALTH_CRITICAL:
+		printf("CRITICAL\n");
+		break;
+	default:
+		printf("UNKNOWN?\n");
+		break;
+	}
+
+	printf("Node Mode ");
+
+	switch(nodeStatus.mode) {
+	case UAVCAN_PROTOCOL_NODESTATUS_MODE_OPERATIONAL:
+		printf("OPERATIONAL\n");
+		break;
+	case UAVCAN_PROTOCOL_NODESTATUS_MODE_INITIALIZATION:
+		printf("INITIALIZATION\n");
+		break;
+	case UAVCAN_PROTOCOL_NODESTATUS_MODE_MAINTENANCE:
+		printf("MAINTENANCE\n");
+		break;
+	case UAVCAN_PROTOCOL_NODESTATUS_MODE_SOFTWARE_UPDATE:
+		printf("SOFTWARE UPDATE\n");
+		break;
+	case UAVCAN_PROTOCOL_NODESTATUS_MODE_OFFLINE:
+		printf("OFFLINE\n");
+		break;
+	default:
+		printf("UNKNOWN?\n");
+		break;
+	}
+}
+
+void handle_NotifyState(CanardInstance *ins, CanardRxTransfer *transfer) {
+	struct ardupilot_indication_NotifyState notifyState;
+
+	if (ardupilot_indication_NotifyState_decode(transfer, &notifyState)) {
+		return;
+	}
+
+	uint32_t nl = notifyState.vehicle_state & 0xFFFFFFFF;  // ignoring the last 32 bits for printing since the highest vehicle_state value right now is 23 even though they're allowed to be up to 64bit unsigned integer
+
+	printf("Vehicle State: %lu ", nl);
+
+	if (notifyState.aux_data.len > 0) {
+		printf("Aux Data: 0x");
+
+		for (int i = 0; i < notifyState.aux_data.len; i++) {
+			printf("%02x", notifyState.aux_data.data[i]);
+		}
+	}
+
+	printf("\n");
+
+}
+
+
+/*
+  handle a GetNodeInfo request
+*/
+// TODO: All the data in here is temporary for testing. If actually need to send valid data, edit accordingly.
+void handle_GetNodeInfo(CanardInstance *ins, CanardRxTransfer *transfer) {
+	printf("GetNodeInfo request from %d\n", transfer->source_node_id);
+
+	uint8_t buffer[UAVCAN_PROTOCOL_GETNODEINFO_RESPONSE_MAX_SIZE];
+	struct uavcan_protocol_GetNodeInfoResponse pkt;
+
+	memset(&pkt, 0, sizeof(pkt));
+
+	node_status.uptime_sec = HAL_GetTick() / 1000ULL;
+	pkt.status = node_status;
+
+	// fill in your major and minor firmware version
+	pkt.software_version.major = 1;
+	pkt.software_version.minor = 0;
+	pkt.software_version.optional_field_flags = 0;
+	pkt.software_version.vcs_commit = 0; // should put git hash in here
+
+	// should fill in hardware version
+	pkt.hardware_version.major = 1;
+	pkt.hardware_version.minor = 0;
+
+	// just setting all 16 bytes to 1 for testing
+	getUniqueID(pkt.hardware_version.unique_id);
+
+	strncpy((char*)pkt.name.data, "SERVONode", sizeof(pkt.name.data));
+	pkt.name.len = strnlen((char*)pkt.name.data, sizeof(pkt.name.data));
+
+	uint16_t total_size = uavcan_protocol_GetNodeInfoResponse_encode(&pkt, buffer);
+
+	canardRequestOrRespond(ins,
+						   transfer->source_node_id,
+						   UAVCAN_PROTOCOL_GETNODEINFO_SIGNATURE,
+						   UAVCAN_PROTOCOL_GETNODEINFO_ID,
+						   &transfer->transfer_id,
+						   transfer->priority,
+						   CanardResponse,
+						   &buffer[0],
+						   total_size);
+}
+
+// Canard Senders
+
+/*
+  send the 1Hz NodeStatus message. This is what allows a node to show
+  up in the DroneCAN GUI tool and in the flight controller logs
+ */
+void send_NodeStatus(void) {
+    uint8_t buffer[UAVCAN_PROTOCOL_GETNODEINFO_RESPONSE_MAX_SIZE];
+
+    node_status.uptime_sec = HAL_GetTick() / 1000UL;
+    node_status.health = UAVCAN_PROTOCOL_NODESTATUS_HEALTH_OK;
+    node_status.mode = UAVCAN_PROTOCOL_NODESTATUS_MODE_OPERATIONAL;
+    node_status.sub_mode = 0;
+
+    // put whatever you like in here for display in GUI
+    node_status.vendor_specific_status_code = 1234;
+
+    uint32_t len = uavcan_protocol_NodeStatus_encode(&node_status, buffer);
+
+    // we need a static variable for the transfer ID. This is
+    // incremeneted on each transfer, allowing for detection of packet
+    // loss
+    static uint8_t transfer_id;
+
+    canardBroadcast(&canard,
+                    UAVCAN_PROTOCOL_NODESTATUS_SIGNATURE,
+                    UAVCAN_PROTOCOL_NODESTATUS_ID,
+                    &transfer_id,
+                    CANARD_TRANSFER_PRIORITY_LOW,
+                    buffer,
+                    len);
+}
+
+// Implement Ardupilot can lighting protocol
+
+void onTransferReceived(CanardInstance *ins, CanardRxTransfer *transfer) {
+  switch (transfer->data_type_id) {
+  // General CAN node functionality
+  case UAVCAN_PROTOCOL_GETNODEINFO_ID: {
+    handle_GetNodeInfo(ins, transfer);
+    break;
+  }
+  case UAVCAN_PROTOCOL_NODESTATUS_ID: {
+    handle_NodeStatus(ins, transfer);
+    break;
+  }
+  case ARDUPILOT_INDICATION_NOTIFYSTATE_ID: {
+    handle_NotifyState(ins, transfer);
+    break;
+  }
+	// Specific efs-can-lighting functionality
+	case UAVCAN_EQUIPMENT_INDICATION_BEEPCOMMAND_ID: {
+	  handle_beep_command(canard_instance, transfer);
+	  break;
+	}
+	case UAVCAN_EQUIPMENT_INDICATION_LIGHTSCOMMAND_ID:
+	  handle_lights_command(canard_instance, transfer);
+	}
+}
+
+bool shouldAcceptTransfer(const CanardInstance *ins,
+                            uint64_t *out_data_type_signature,
+                            uint16_t data_type_id,
+                            CanardTransferType transfer_type,
+                            uint8_t source_node_id)
+{
+  
+  switch (data_type_id) {
+  // General CAN node functionality
+  case UAVCAN_PROTOCOL_GETNODEINFO_ID: {
+    *out_data_type_signature = UAVCAN_PROTOCOL_GETNODEINFO_REQUEST_SIGNATURE;
+    return true;
+  }
+  case UAVCAN_PROTOCOL_NODESTATUS_ID: {
+    *out_data_type_signature = UAVCAN_PROTOCOL_NODESTATUS_SIGNATURE;
+    return true;
+  }
+  case ARDUPILOT_INDICATION_NOTIFYSTATE_ID: {
+    *out_data_type_signature = ARDUPILOT_INDICATION_NOTIFYSTATE_SIGNATURE;
+    return true;
+  }
+	// Specific efs-can-lighting functionality
+	case UAVCAN_EQUIPMENT_INDICATION_BEEPCOMMAND_ID: {
+	  *out_data_type_signature = UAVCAN_EQUIPMENT_INDICATION_BEEPCOMMAND_ID;
+	  return true;
+	}
+	case UAVCAN_EQUIPMENT_INDICATION_LIGHTSCOMMAND_ID:
+	  *out_data_type_signature = UAVCAN_EQUIPMENT_INDICATION_LIGHTSCOMMAND_ID;
+	  return true;
+	}
+
+	// ignore all other kinds of messages
+	return false;
+}
+
+void processCanardTxQueue(FDCAN_HandleTypeDef *hfdcan) {
+	// Transmitting
+
+	for (const CanardCANFrame *tx_frame ; (tx_frame = canardPeekTxQueue(&canard)) != NULL;) {
+		const int16_t tx_res = canardSTM32Transmit(hfdcan, tx_frame);
+
+		if (tx_res < 0) {
+			printf("Transmit error %d\n", tx_res);
+		} else if (tx_res > 0) {
+			printf("Successfully transmitted message\n");
+		}
+
+		// Pop canardTxQueue either way
+		canardPopTxQueue(&canard);
+	}
+}
+
+/*
+  This function is called at 1 Hz rate from the main loop.
+*/
+void process1HzTasks(uint64_t timestamp_usec) {
+    /*
+      Purge transfers that are no longer transmitted. This can free up some memory
+    */
+    canardCleanupStaleTransfers(&canard, timestamp_usec);
+
+    /*
+      Transmit the node status message
+    */
+    send_NodeStatus();
+}
+
+// TODO: implement these
+void handle_beep_command(CanardInstance *ins, CanardRxTransfer *transfer) {}
+void handle_lights_command(CanardInstance *ins, CanardRxTransfer *transfer) {}
+
 
 /* USER CODE END 0 */
 
@@ -94,6 +397,18 @@ int main(void)
   MX_CAN1_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
+  canardInit(&canard, memory_pool, sizeof(memory_pool), onTransferReceived, shouldAcceptTransfer, NULL);
+  uint64_t next_1hz_service_at = HAL_GetTick();
+  uint64_t next_50hz_service_at = HAL_GetTick();
+
+  // static node allocation
+  if (NODE_ID > 0) {
+	  canardSetLocalNodeID(&canard, NODE_ID);
+  } else {
+	  printf("Node ID is 0, this node is anonymous and can't transmit most messaged. Please update this in node_settings.h\n");
+  }
+
+
   uint32_t hex_color = 0x000339FC;
   led_set_HEX(hex_color);
   led_render();
@@ -104,8 +419,19 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+  
     /* USER CODE BEGIN 3 */
+    processCanardTxQueue(&hfdcan1);
+
+    const uint64_t ts = HAL_GetTick();
+    if (ts >= next_1hz_service_at) {
+      next_1hz_service_at += 1000ULL;
+      process1HzTasks(ts);
+    }
+    if (ts >= next_50hz_service_at) {
+      next_50hz_service_at += 1000000ULL/50U;
+      // TODO: send neopixel status info back to flight controller here, see efs-can-servo for example using servos
+    }
   }
   /* USER CODE END 3 */
 }
