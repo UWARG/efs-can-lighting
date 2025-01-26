882d9a6265b78cfe72c774e9a549ac0a6e2d7ecc
n-arms <75282394+n-arms@users.noreply.github.com>
2025-01-26 16:43:46 -0500

    Add  and detail leader selection algorithm

--- Core/Inc/can_state.hpp
+++ Core/Inc/can_state.hpp
@@ -8,10 +8,6 @@
 #ifndef INC_CAN_STATE_HPP_
 #define INC_CAN_STATE_HPP_
 
-
-
-#include <variant>
-
 class NodeState {
 private:
   NodeStateValue value;
@@ -21,29 +17,33 @@ private:
 union NodeStateValue {
   NodeMasterState master;
   NodeFollowerState follower;
+  NodeCandidateState candidate;
 };
 
 enum NodeStateTag {
-  NODE_MASTER_STATE, NODE_FOLLOWER_STATE
+  NODE_MASTER_STATE, NODE_FOLLOWER_STATE, NODE_CANDIDATE_STATE
 };
 
 class NodeMasterState {
 private:
-  // if the time since last ardupilot packet exceeds MAX_PACKET_DELAY, this node gives up its role as master, broadcasting an appropriate message
+  // if the time since last ardupilot packet exceeds `MAX_PACKET_DELAY`, this node gives up its role as master, broadcasting a `MASTER_DEATH` packet
   uint32_t last_ardupilot_packet;
   static constexpr uint32_t MAX_PACKET_DELAY;
 };
 
 class NodeFollowerState {
 private:
-  // if the time since last master packet exceeds MAX_PACKET_DELAY, this node transitions to `NodeProspectiveState` where it either transitions back to a `NodeFollowerState` or `NodeMasterState`
-  // if the node receives a `NodeMasterState`, it transitions to a `NodeProspectiveState`
+  // if the time since last master packet exceeds `MAX_PACKET_DELAY`, this node transitions to `NodeCandidateState` where it either transitions back to a `NodeFollowerState` or `NodeMasterState`
+  // if the node receives a `NodeMasterState`, it transitions to a `NodeCandidateState`
   uint32_t last_master_packet;
   static constexpr uint32_t MAX_PACKET_DELAY;
 };
 
-class NodeProspectiveState {
+// A `NodeCandidateState` sends out an `START_ELECTION` packet. If after `ELECTION_DURATION` ticks the candidate hasn't received an `ELECTION_DURATION` from a node with a lower ID, it broadcasts a `ASSERT_MASTER` message and transitions to a `NodeMasterState`.
+class NodeCandidateState {
 private:
+  uint32_t election_start;
+  static constexpr uint32_t ELECTION_DURATION;
 };
 
 #endif
