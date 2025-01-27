a889940b04d8e706f3a2995e1c9a3ff03fa2c687
n-arms <75282394+n-arms@users.noreply.github.com>
2025-01-27 18:30:30 -0500

    Fix bugs in leader election algorithm, get code compiling

--- Core/Inc/can_state.hpp
+++ Core/Inc/can_state.hpp
@@ -8,17 +8,56 @@
 #ifndef INC_CAN_STATE_HPP_
 #define INC_CAN_STATE_HPP_
 
-struct Packet {
-  PacketKind kind;
-  uint32_t id;
-};
+#include <cstdint>
 
 enum PacketKind {
   ARDUPILOT_PACKET, INTERNAL_PACKET, ASSERT_MASTER_PACKET
 };
 
+struct Packet {
+  PacketKind kind;
+  uint32_t id;
+};
+
 constexpr uint32_t MAX_ARDUPILOT_PACKET_DELAY = 5000;
 constexpr uint32_t MAX_MASTER_PACKET_DELAY = 100;
+
+class NodeState;
+
+// if the time since last ardupilot packet exceeds `MAX_ARDUPILOT_PACKET_DELAY`, the node gives up its status as master
+// additionally, if it recieves an ASSERT_MASTER packet from a node with a lower ID, it gives up its status as master
+class NodeMasterState {
+public:
+  NodeMasterState(uint32_t id, uint32_t last_ardupilot_packet);
+  void handlePacket(Packet packet, uint32_t time);
+  bool update(NodeState &state, PacketKind &toSend, uint32_t time);
+private:
+  uint32_t id;
+  uint32_t last_ardupilot_packet;
+  bool should_transition = false;
+};
+
+// if the time since last master packet exceeds `MAX_MASTER_PACKET_DELAY` and an ardupilot packet has been seen recently, it sends an ASSERT_MASTER packet and becomes a master
+class NodeFollowerState {
+public:
+  NodeFollowerState(uint32_t id, uint32_t last_master_packet, uint32_t last_ardupilot_packet);
+  void handlePacket(Packet packet, uint32_t time);
+  bool update(NodeState &state, PacketKind &toSend, uint32_t time);
+private:
+  uint32_t id;
+  uint32_t last_master_packet;
+  uint32_t last_ardupilot_packet;
+  bool should_transition = false;
+};
+
+enum NodeStateTag {
+  NODE_MASTER_STATE, NODE_FOLLOWER_STATE
+};
+
+union NodeStateValue {
+  NodeMasterState master;
+  NodeFollowerState follower;
+};
 
 class NodeState {
 public:
@@ -34,38 +73,4 @@ private:
   NodeStateValue value;  
 };
 
-enum NodeStateTag {
-  NODE_MASTER_STATE, NODE_FOLLOWER_STATE
-};
-
-union NodeStateValue {
-  NodeMasterState master;
-  NodeFollowerState follower;
-};
-
-// if the time since last ardupilot packet exceeds `MAX_ARDUPILOT_PACKET_DELAY`, the node gives up its status as master
-// additionally, if it recieves an ASSERT_MASTER packet from a node with a lower ID, it gives up its status as master
-class NodeMasterState {
-public:
-  NodeState(uint32_t id, uint32_t last_ardupilot_packet);
-  void handlePacket(Packet packet, uint32_t time);
-  bool update(NodeState &state, PacketKind &toSend, uint32_t time);
-private:
-  const uint32_t id;
-  uint32_t last_ardupilot_packet;
-  bool should_transition = false;
-};
-
-// if the time since last master packet exceeds `MAX_MASTER_PACKET_DELAY` and an ardupilot packet has been seen recently, it sends an ASSERT_MASTER packet and becomes a master
-class NodeFollowerState {
-public:
-  NodeFollowerState(uint32_t id, uint32_t last_master_packet, uint32_t last_ardupilot_packet);
-  void handlePacket(Packet packet, uint32_t time) override;
-  bool update(NodeState &state, PacketKind &toSend, uint32_t time);
-private:
-  const uint32_t id;
-  uint32_t last_master_packet;
-  uint32_t last_ardupilot_packet;
-  bool should_transition = false;
-};
 #endif
--- Core/Src/can_state.cpp
+++ Core/Src/can_state.cpp
@@ -7,10 +7,8 @@
 
 #include "can_state.hpp"
 
-NodeState::NodeState(uint32_t id, uint32_t time): tag(NODE_FOLLOWER_STATE) {
-  value.follower = NodeFollowerState(id, time, time);
-}
-NodeState::handlePacket(Packet packet, uint32_t time) {
+NodeState::NodeState(uint32_t id, uint32_t time): tag(NODE_FOLLOWER_STATE), value {.follower = NodeFollowerState(id, time, time)} {}
+void NodeState::handlePacket(Packet packet, uint32_t time) {
   switch (tag) {
     case NODE_MASTER_STATE: {
       value.master.handlePacket(packet, time);
@@ -25,35 +23,36 @@ NodeState::handlePacket(Packet packet, uint32_t time) {
 bool NodeState::update(PacketKind &toSend, uint32_t time) {
   switch (tag) {
     case NODE_MASTER_STATE: {
-      value.master.update(this, packet, time);
-      break;
+      return value.master.update(*this, toSend, time);
     }
     case NODE_FOLLOWER_STATE: {
-      value.follower.update(this, packet, time);
-      break;
+      return value.follower.update(*this, toSend, time);
+    }
+    default: {
+      return false;
     }
   }
 }
 void NodeState::transition(NodeMasterState state) {
   tag = NODE_MASTER_STATE;
-  tag.value.master = state;
+  value.master = state;
 }
 void NodeState::transition(NodeFollowerState state) {
   tag = NODE_FOLLOWER_STATE;
-  tag.value.follower = state;
+  value.follower = state;
 }
 
 NodeMasterState::NodeMasterState(uint32_t id, uint32_t last_ardupilot_packet): id(id), last_ardupilot_packet(last_ardupilot_packet) {}
-void NodeMasterState::handlePacket(Packet packet, uint32_t time) override {
+void NodeMasterState::handlePacket(Packet packet, uint32_t time) {
   if (packet.kind == ARDUPILOT_PACKET) {
     last_ardupilot_packet = time;
-  } else if (packet.kind == ASSERT_MASTER && id > packet.id) {
+  } else if (packet.kind == ASSERT_MASTER_PACKET && id > packet.id) {
     should_transition = true;
   }
 }
 bool NodeMasterState::update(NodeState &state, PacketKind &toSend, uint32_t time) {
   if (should_transition || last_ardupilot_packet + MAX_ARDUPILOT_PACKET_DELAY < time) {
-    state.transition(NodeFollowerState(id, time, last_ardupilot_package));
+    state.transition(NodeFollowerState(id, time, last_ardupilot_packet));
     return true;
   } else {
     return false;
@@ -61,10 +60,10 @@ bool NodeMasterState::update(NodeState &state, PacketKind &toSend, uint32_t time
 }
 
 NodeFollowerState::NodeFollowerState(uint32_t id, uint32_t last_master_packet, uint32_t last_ardupilot_packet): id(id), last_master_packet(last_master_packet), last_ardupilot_packet(last_ardupilot_packet) {}
-void NodeFollowerState::handlePacket(Packet packet, uint32_t time) override {
+void NodeFollowerState::handlePacket(Packet packet, uint32_t time) {
   if (packet.kind == ARDUPILOT_PACKET) {
     last_ardupilot_packet = time;
-  } else if (packet.kind == INTERNAL_PACKET || packet.kind == ASSERT_MASTER) {
+  } else if (packet.kind == INTERNAL_PACKET || packet.kind == ASSERT_MASTER_PACKET) {
     last_master_packet = time;
   } else if (last_master_packet + MAX_MASTER_PACKET_DELAY < time && last_ardupilot_packet + MAX_ARDUPILOT_PACKET_DELAY > time) {
     should_transition = true;
@@ -72,8 +71,8 @@ void NodeFollowerState::handlePacket(Packet packet, uint32_t time) override {
 }
 bool NodeFollowerState::update(NodeState &state, PacketKind &toSend, uint32_t time) {
   if (should_transition) {
-    state.transition(NodeCandidateState(id, time));
-    toSend = ASSERT_MASTER;
+    state.transition(NodeMasterState(id, last_ardupilot_packet));
+    toSend = ASSERT_MASTER_PACKET;
     return true;
   } else {
     return false;
