83e46a5d3e524925f817f55ea3f9b4a62299b546
bkarantz <billyk777@bell.net>
2025-03-24 19:13:18 -0400

    Lua script pending altitude and CAN send functions

--- Lua_CAN_Script.lua
+++ Lua_CAN_Script.lua
@@ -29,14 +29,15 @@ end
 local can_message_ID = uint32_t(10) --sets CAN ID
 local can_bus = uint32_t(1) -- CAN bus to use
 
+
 local function get_control_state() --function to get the current control state of the drone
     if arming:arm() == true and arming:is_armed() == false then --check if drone is ready to arm bus isn't
         drone_state = states.ground
     elseif arming:is_armed() == true and motors:get_throttle() == 0 then --check if drone is armed but throttle is 0
         drone_state = states.taxi
-    elseif motors:get_throttle() ~= 0 and ahrs:altitude() < 5 then --check if drone is below 5m but throttle not 0
+    elseif motors:get_throttle() ~= 0 and Location_ud:alt() < 50 then --check if drone is below 5m but throttle not 0
         drone_state = states.takeoff
-    elseif ahrs:altitude() >= 5 and arming:is_armed() == true then --check if drone above 5m
+    elseif Location_ud:alt() >= 50 and arming:is_armed() == true then --check if drone above 5m (location_ud class gives error)
         drone_state = states.flight
     elseif vehicle:is_landing() == true then --check if drone is landing
         drone_state = states.landing
@@ -46,14 +47,16 @@ local function get_control_state() --function to get the current control state o
 
     local message = CANFrame() --Create new CAN frame
     message:id(can_message_ID) -- Set CAN message ID
+    message:dlc(1) -- Set data length
     message:data(0, drone_state) -- Set data for drone state
 
-    if can.send({id = message:id(), ext = false, data = {message:data(0)}}) then --Send CAN frame
-        gcs:send_text(6, string.format("Drone state: %d", drone_state)) -- display drone state
+    if ScriptingCANBuffer_ud:write_frame(message, 1000) then --Send CAN frame (Error here, doesn't recognize class)
+        gcs:send_text(6, string.format("Drone state: %d", drone_state))-- display drone state
     else
         gcs:send_text(3, "CAN Send Failed!")
     end
-    return get_control_state(), 100
+    gcs:send_text(0, "Text")
+    return get_control_state, 1000
         
 end
 return get_control_state()
